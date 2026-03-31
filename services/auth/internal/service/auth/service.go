a28af07c03efa06b24de8c20557678d26f4226bc
deniSSTK <denisdrawler@gmail.com>
2026-03-31 12:33:03 +0300

    Extract `generateAndStoreTokens` method to a new `tokens.go` file for better separation of concerns within the auth service.

--- services/auth/internal/service/auth/service.go
+++ services/auth/internal/service/auth/service.go
@@ -159,29 +159,3 @@ func (s *Service) Login(ctx context.Context, dto *proto.LoginRequest) (*jwt.Toke
 }
 
 func (s *Service) Refresh(context.Context, *proto.RefreshRequest) (*proto.TokensResponse, error) {}
-
-func (s *Service) generateAndStoreTokens(
-	ctx context.Context,
-	payload jwt.TokenPayload,
-) (*jwt.TokenPair, error) {
-	tokens, err := s.tokenManager.GenerateBothTokens(payload)
-	if err != nil {
-		s.log.Error(FailedToGenerateTokens.Error(), zap.Error(err))
-		return &jwt.TokenPair{}, err
-	}
-
-	// TODO: create db session with deviceId
-
-	cachePayload := UserSessionCachePayload{
-		UserId:       payload.UserId,
-		RefreshToken: tokens.RefreshToken,
-		ExpiredAt:    s.config.JWT.RefreshTokenTTL,
-	}
-
-	if err = s.saveUserSessionCache(ctx, &cachePayload); err != nil {
-		s.log.Error(FailedToSaveUserSessionCache.Error(), zap.Error(err))
-		return &jwt.TokenPair{}, FailedToSaveUserSessionCache
-	}
-
-	return tokens, nil
-}
---
+++ services/auth/internal/service/auth/tokens.go
@@ -0,0 +1,34 @@
+package authService
+
+import (
+	"auth-service/internal/infra/security/jwt"
+	"context"
+
+	"go.uber.org/zap"
+)
+
+func (s *Service) generateAndStoreTokens(
+	ctx context.Context,
+	payload jwt.TokenPayload,
+) (*jwt.TokenPair, error) {
+	tokens, err := s.tokenManager.GenerateBothTokens(payload)
+	if err != nil {
+		s.log.Error(FailedToGenerateTokens.Error(), zap.Error(err))
+		return &jwt.TokenPair{}, err
+	}
+
+	// TODO: create db session with deviceId
+
+	cachePayload := UserSessionCachePayload{
+		UserId:       payload.UserId,
+		RefreshToken: tokens.RefreshToken,
+		ExpiredAt:    s.config.JWT.RefreshTokenTTL,
+	}
+
+	if err = s.saveUserSessionCache(ctx, &cachePayload); err != nil {
+		s.log.Error(FailedToSaveUserSessionCache.Error(), zap.Error(err))
+		return &jwt.TokenPair{}, FailedToSaveUserSessionCache
+	}
+
+	return tokens, nil
+}
